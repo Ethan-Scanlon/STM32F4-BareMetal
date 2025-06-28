b974bc240b7587f62991f65ba138eae7578319aa
Your Name <you@example.com>
2025-06-27 17:47:54 -0700

    Debug UART Driver

--- 5_uart_tx/Src/main.c
+++ 5_uart_tx/Src/main.c
@@ -1,28 +1,33 @@
 #include "stm32f4xx.h"
+#include <stdint.h>
 
 #define GPIOAEN			(1U<<0)
 #define UART2EN			(1U<<17)
 
 #define CR1_TE			(1U<<3)
 #define CR1_UE			(1U<<13)
+#define SR_TXE			(1U<<7)
 
 #define SYS_FREQ		16000000
-#define APB1_CLK		SYS_FREQ
+#define APB1_CLK		(SYS_FREQ/2U)
 
 #define UART_BAUDRATE	115200
 
 
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate);
-static uint16_t computer_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
 
-void uar2_tx_init(void);
+void uart2_tx_init(void);
+void uart2_write(int ch);
 
 int main(void) {
 
+	uart2_tx_init();
 
 	while(1) {
-
+		uart2_write('Y');
+		for (volatile int i=0; i<100000; i++);
 	}
 }
 
@@ -58,17 +63,18 @@ void uart2_tx_init(void) {
 void uart2_write(int ch) {
 
 	/*Make sure the transmit data register is empty before we put somehting into it*/
+	while(!(USART2->SR & SR_TXE)) {}
 	/*Write to transmit data register*/
-
 	USART2->DR = (ch & 0xFF);
+	//Turn into 8 bit?
 
 }
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate) {
 	USARTx->BRR = compute_uart_bd(PeriphClk, BaudRate);
 }
 
-static uint16_t computer_uart_bd(uint32_t PeriphClk, uint32_t BaudRate) {
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate) {
 
-	return (PeriphClk + (BaudRate/2U)/BaudRate);
+	return (uint16_t)((PeriphClk + (BaudRate/2U))/BaudRate);
 
 }
